deaf2fd6da6c5c37049c8cd22f0cc21e631d269a
bellagomez <Amarag24@gmail.com>
2017-07-17 17:02:16 -0500

    Random colors

--- java script.js
+++ java script.js
@@ -1,28 +1,39 @@
-var 
+var the = document.querySelector('body');
 
 
 
 
+var counter = 0;
+while(counter < 4) {
+    var thhe = document.createElement('div');
+
+    var red = getRandomInt(0,255);
+    var green = getRandomInt(0,255);
+    var blue = getRandomInt(0,255);
+
+
+    var d = getRandomInt(0,255);
+    var a = getRandomInt(0,255);
+    var f = getRandomInt(0,255);
+
+
+thhe.style.borderColor = `rgb(${d}, ${a}, ${f}`;
+    thhe.style.backgroundColor = `rgb(${red}, ${green}, ${blue}`;
+    the.appendChild(thhe);
+
+    counter += 1;
+
+}
 
 
 
+function getRandomInt(min,max) {
+    return Math.floor(Math.random() * (max - min + 1)) +min;
+}
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+                                     
 
 
 
